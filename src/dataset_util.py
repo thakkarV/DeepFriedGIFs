5ec0786b71ea0f5af2590f5c4d63527e6c788fd1
thakkarv <thakkarv@bu.edu>
2018-11-12 16:01:58 -0500

    add boilerplate code for gif cropping

--- src/dataset_util.py
+++ src/dataset_util.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import fnmatch
 import os
+from sys import getsizeof
 
 def find_files(directory, pattern):
     '''Recursively finds all files matching the pattern.'''
@@ -13,11 +14,23 @@ def find_files(directory, pattern):
     return files
 
 class Dataset(object):
-    def __init__(self, datadir):
+    def __init__(self, datadir, crop=False):
         self.datadir = datadir
         self.files   = None
+        self.crop    = crop
 
-    def gif_generator(self, batch_size=1):
+    def gif_generator(
+        self,
+        batch_size=1,
+        x_len=None,
+        y_len=None,
+        crop_pos=None):
+
+        if (self.crop):
+            assert x_len is not None
+            assert y_len is not None
+            assert crop_pos is not None
+
         if self.files is None:
             self.files = find_files(self.datadir, "*.gif")
         random.shuffle(self.files)
@@ -29,6 +42,11 @@ class Dataset(object):
             palette_batch = []
             for i in range(batch_size):
                 frames, palette = self.get_frames(self.files[gif_idx])
+                
+                # now crop frames if needed
+                if (self.crop):
+                    frames = self.crop_frames(frames, crop_pos, x_len, y_len)
+
                 frame_batch.append(frames)
                 palette_batch.append(palette)
                 gif_idx += 1
@@ -50,14 +68,45 @@ class Dataset(object):
                 frames.append(np.array(frame))
         except EOFError:
             gif.close()
-            return frames, np.array(palette)
+            return np.array(frames), np.array(palette)
+
+    def crop_frames(self, frames, pos, x_len, y_len):
+        if   pos == "UL":
+            # crop and keep upper left
+            raise NotImplementedError("")
+            return frames
+        elif pos == "LL":
+            # crop and keep lower left
+            raise NotImplementedError("")
+            return frames
+        elif pos == "UR":
+            # crop and keep upper right
+            raise NotImplementedError("")
+            return frames
+        elif pos == "LR":
+            # crop and keep lower right
+            raise NotImplementedError("")
+            return frames
+        elif pos == "CC":
+            # crop and keep center
+            raise NotImplementedError("")
+            return frames
+        else:
+            raise Exception("Invalid crop position.")
 
 
 def test(path):
     dataset = Dataset(path)
     for gif, palette in dataset.gif_generator():
-        print(gif)
-        print(palette)
+        print(len(gif))
+        print(gif[0].shape)
+        print(len(palette[0]))
+        print(palette[0].dtype)
+        ar = np.array(palette[0], dtype=np.int8)
+        print(ar.dtype)
+        print(getsizeof(palette[0]))
+        print(getsizeof(ar))
+
         # frame.save("test{}.png".format(idx), **frame.info)
 
 if __name__ == "__main__":
