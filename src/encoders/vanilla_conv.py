3da4a1682354085cbc697692dd75e02bfd7a928c
Karanraj Chauhan <chauhank@bu.edu>
2018-11-18 13:21:36 -0500

    Added biases to conv layers in vanilla encoder. Replaced explicit relu activation call with relu activation using conv layer parameter

--- src/encoders/vanilla_conv.py
+++ src/encoders/vanilla_conv.py
@@ -29,9 +29,9 @@ def vanilla_2d_conv(x, args, reuse=False):
                                         kernel_size=3,
                                         strides=1,
                                         padding='same',
-                                        use_bias=False)
-            relu1 = tf.nn.relu(conv1)
-            pool1 = tf.layers.max_pooling2d(inputs=relu1, pool_size=2, strides=2)
+                                        use_bias=True,
+                                        activation=tf.nn.relu)
+            pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=2, strides=2)
         # 128 conv -> relu -> pool
         with tf.variable_scope("conv_layers_2"):
             conv2 = tf.layers.conv2d(inputs=pool1, 
@@ -39,9 +39,9 @@ def vanilla_2d_conv(x, args, reuse=False):
                                         kernel_size=3,
                                         strides=1,
                                         padding='same', 
-                                        use_bias=False)
-            relu2 = tf.nn.relu(conv2)
-            pool2 = tf.layers.max_pooling2d(inputs=relu2, pool_size=2, strides=2)
+                                        use_bias=True,
+                                        activation=tf.nn.relu)
+            pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=2, strides=2)
         # 256 conv -> relu -> 256 conv -> relu -> pool
         with tf.variable_scope("conv_layers_3"):
             conv3_1 = tf.layers.conv2d(inputs=pool2, 
@@ -49,16 +49,15 @@ def vanilla_2d_conv(x, args, reuse=False):
                                             kernel_size=3, 
                                             strides=1, 
                                             padding='same',
-                                            use_bias=False)
-            relu3_1 = tf.nn.relu(conv3_1)
-            conv3_2 = tf.layers.conv2d(inputs=relu3_1, 
+                                            use_bias=True,
+                                            activation=tf.nn.relu)
+            conv3_2 = tf.layers.conv2d(inputs=conv3_1, 
                                             filters=256, 
                                             kernel_size=3, 
                                             strides=1, 
                                             padding='same',
-                                            use_bias=False)
-            relu3_2 = tf.nn.relu(conv3_2)
-            pool3 = tf.layers.max_pooling2d(inputs=relu3_2, pool_size=2, strides=2)
+                                            use_bias=True, activation=tf.nn.relu)
+            pool3 = tf.layers.max_pooling2d(inputs=conv3_2, pool_size=2, strides=2)
         # 512 conv -> relu -> 512 conv -> relu -> pool
         with tf.variable_scope("conv_layers_4"):
             conv4_1 = tf.layers.conv2d(inputs=pool3, 
@@ -66,16 +65,16 @@ def vanilla_2d_conv(x, args, reuse=False):
                                             kernel_size=3, 
                                             strides=1, 
                                             padding='same',
-                                            use_bias=False)
-            relu4_1 = tf.nn.relu(conv4_1)
-            conv4_2 = tf.layers.conv2d(inputs=relu4_1, 
+                                            use_bias=True, 
+                                            activation=tf.nn.relu)
+            conv4_2 = tf.layers.conv2d(inputs=conv4_1, 
                                             filters=512, 
                                             kernel_size=3, 
                                             strides=1, 
                                             padding='same',
-                                            use_bias=False)
-            relu4_2 = tf.nn.relu(conv4_2)
-            pool4 = tf.layers.max_pooling2d(inputs=relu4_2, pool_size=2, strides=2)
+                                            use_bias=True,
+                                            activation=tf.nn.relu)
+            pool4 = tf.layers.max_pooling2d(inputs=conv4_2, pool_size=2, strides=2)
         # 512 conv -> relu -> 512 conv -> relu -> pool -> flatten
         with tf.variable_scope("conv_layers_5"):
             conv5_1 = tf.layers.conv2d(inputs=pool4, 
@@ -83,24 +82,21 @@ def vanilla_2d_conv(x, args, reuse=False):
                                             kernel_size=3, 
                                             strides=1,
                                             padding='same',
-                                            use_bias=False)
-            relu5_1 = tf.nn.relu(conv5_1)
-            conv5_2 = tf.layers.conv2d(inputs=relu5_1, 
+                                            use_bias=True,
+                                            activation=tf.nn.relu)
+            conv5_2 = tf.layers.conv2d(inputs=conv5_1, 
                                             filters=512, 
                                             kernel_size=3, 
                                             strides=1, 
                                             padding='same',
-                                            use_bias=False)
-            relu5_2 = tf.nn.relu(conv5_2)
-            pool5 = tf.layers.max_pooling2d(inputs=relu5_2, pool_size=2, strides=2)
+                                            use_bias=True,
+                                            activation=tf.nn.relu)
+            pool5 = tf.layers.max_pooling2d(inputs=conv5_2, pool_size=2, strides=2)
             pool5_flattened = tf.contrib.layers.flatten(pool5)
         # 4096 fc -> 4096 fc -> 1000 fc -> args.z_dim z
         with tf.variable_scope("fc_layers"):
-            fc1 = tf.layers.dense(inputs=pool5_flattened ,units=4096)
-            relu_fc1 = tf.nn.relu(fc1)
-            fc2 = tf.layers.dense(inputs=relu_fc1 ,units=4096)
-            relu_fc2 = tf.nn.relu(fc2)
-            fc3 = tf.layers.dense(inputs=relu_fc2 ,units=1000)
-            relu_fc3 = tf.nn.relu(fc3)
-            z = tf.layers.dense(inputs=relu_fc3, units=args.z_dim)
+            fc1 = tf.layers.dense(inputs=pool5_flattened, units=4096, activation=tf.nn.relu)
+            fc2 = tf.layers.dense(inputs=fc1 ,units=4096, activation=tf.nn.relu)
+            fc3 = tf.layers.dense(inputs=fc2 ,units=1000, activation=tf.nn.relu)
+            z = tf.layers.dense(inputs=fc3, units=args.z_dim)
     return z
