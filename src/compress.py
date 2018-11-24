6429a7b300f8548d8997e91712422ed80c74e0bf
thakkarv <thakkarv@bu.edu>
2018-11-24 13:32:19 -0500

    add preamble for inference from encoders -- compression

--- src/compress.py
+++ src/compress.py
@@ -1,9 +1,33 @@
-from arg_parser import parse_compress_args
+import os
+
 import encoders
+from dataset_util import Dataset
+from arg_parser import parse_compress_args
 
 def compress(args):
+    # validate encoder
     encoder = getattr(encoders, args.encoder)
-    pass
+
+    # validate input GIF and load it
+    if not os.path.exists(args.data) and not os.path.isfile(args.data):
+        raise ValueError("Could not stat input GIF file.")
+
+    frames, palette = Dataset.load_gif(args.data)
+    if frames is None:
+        raise ValueError(
+            "Could not read GIF file at path {}".format(args.data))
+
+    # crop if needed
+    if args.crop_pos is not None:
+        assert args.crop_height is not None
+        assert args.crop_width  is not None
+
+        frames = Dataset.crop_frames(
+            frames,
+            args.crop_pos,
+            args.crop_height,
+            args.crop_width
+        )
 
 if __name__ == "__main__":
     args = parse_compress_args()
