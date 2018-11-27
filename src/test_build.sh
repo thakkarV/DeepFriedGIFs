6755a3a34ab1e26e08bd3efa4c17a92091c4c6ee
thakkarv <thakkarv@bu.edu>
2018-11-27 18:13:56 -0500

    update training sh script to load required modules

--- src/test_build.sh
+++ src/test_build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+module load python/3.6.2
+# module load cuda/9.1
+# module load cudnn/7.1
+module load tensorflow/r1.10
+
 python train.py \
 -e vanilla_encoder \
 -d vanilla_decoder \
@@ -7,5 +12,5 @@ python train.py \
 -o 0 \
 -cp CC \
 -m ../runs/tests/vanilla/ \
---data-dir ../data/train \
+--data ../data/train \
 --n-epoch 1
