Updated 16 bit tensors to 32 bit - fixed nan weights bug. Added commented code to inspect gradients and weights after each iter (possible TODO: add debug argument to arg parser, uncomment the debug code and put it under if args.debug==True

src/dataset_util.py
@@ -219,7 +219,7 @@ class Dataset(object):
         if self.window_size == 1:
             frame_batch = np.empty(
                 shape = (self.batch_size, gif_height, gif_width, 1),
-                dtype = np.float16
+                dtype = np.float32
             )
         else:
             frame_batch = np.empty(
@@ -229,12 +229,12 @@ class Dataset(object):
                     gif_height,
                     gif_width,
                     1),
-                dtype = np.float16
+                dtype = np.float32
             )
 
         target_batch = np.empty(
             shape = (self.batch_size, gif_height, gif_width, 1),
-            dtype = np.float16
+            dtype = np.float32
         )
 
         palette_batch = np.empty(
@@ -252,20 +252,20 @@ class Dataset(object):
             slice_idx = curator_state[i, S_GIF_CUR_IDX]
             if self.window_size == 1:
                 frame_batch[i, :, :, :] = np.expand_dims(
-                    frames[slice_idx, :, :].astype(np.float16),
+                    frames[slice_idx, :, :].astype(np.float32),
                     axis = 2
                 )
             else:
                 frame_batch[i, :, :, :] = np.expand_dims(
                     frames[slice_idx : slice_idx+self.window_size, :, :]
-                        .astype(np.float16),
+                        .astype(np.float32),
                     axis = 3
                 )
 
             # target
             target_idx = curator_state[i, S_GIF_CUR_IDX] + self.target_offset
             target_batch[i, :, :] = np.expand_dims(
-                frames[target_idx, :, :].astype(np.float16),
+                frames[target_idx, :, :].astype(np.float32),
                 axis = 2
             )
 

src/train.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import tensorflow as tf
 
 import encoders
@@ -6,6 +7,8 @@ import decoders
 from arg_parser import parse_train_args
 from dataset_util import Dataset
 from loss import reconstruction_loss
+
+import pdb
 
 
 def train(args):
@@ -30,7 +33,7 @@ def train(args):
     with tf.Graph().as_default() as g:
         # placeholders
         # target frame, number of channels is always one for GIF
-        T = tf.placeholder(tf.float16, shape=(
+        T = tf.placeholder(tf.float32, shape=(
             args.batch_size,
             args.crop_height,
             args.crop_width,
@@ -40,7 +43,7 @@ def train(args):
         if args.crop_pos is not None:
             # non FCN case
             if args.window_size > 1:
-                X = tf.placeholder(tf.float16, shape=(
+                X = tf.placeholder(tf.float32, shape=(
                     args.batch_size,
                     args.window_size,
                     args.crop_height,
@@ -48,7 +51,7 @@ def train(args):
                     1)
                 )
             else:
-                X = tf.placeholder(tf.float16, shape=(
+                X = tf.placeholder(tf.float32, shape=(
                     args.batch_size,
                     args.crop_height,
                     args.crop_width,
@@ -68,8 +71,9 @@ def train(args):
 
         # optimizer
         with tf.name_scope("optim"):
-            train_op = tf.train.AdamOptimizer(
-                args.learning_rate).minimize(loss_op)
+            optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
+            # grads = optimizer.compute_gradients(loss_op)
+            train_op = optimizer.minimize(loss_op)
 
         # summaries
         with tf.name_scope("summary"):
@@ -82,6 +86,7 @@ def train(args):
             init_op = tf.global_variables_initializer()
 
     # graph execution
+    print('starting training with learning rate = {}'.format(args.learning_rate))
     with tf.Session(graph=g) as sess:
         summary_writer = tf.summary.FileWriter(
             os.path.join(args.save_path, "log"), sess.graph)
@@ -111,6 +116,18 @@ def train(args):
                 for input_frames, target_frames, palettes in \
                     dataset.generate_training_batch():
 
+#                    # pinpoint where the nans in encoder network
+#                    for var in g.get_collection(name=tf.GraphKeys.GLOBAL_VARIABLES, scope='encoder'):
+#                        if tf.is_nan(var).eval().any():
+#                            print('{} has nan values'.format(var.name))
+#                            pdb.set_trace()
+
+#                    # pinpoint where the nans in decoder network
+#                    for var in g.get_collection(name=tf.GraphKeys.GLOBAL_VARIABLES, scope='decoder'):
+#                        if tf.is_nan(var).eval().any():
+#                            print('{} has nan values'.format(var.name))
+#                            pdb.set_trace()
+
                     loss, _, summary = sess.run(
                         [loss_op, train_op, summary_op],
                         feed_dict={
@@ -118,11 +135,23 @@ def train(args):
                             T: target_frames
                         }
                     )
+
+#                    # check if the encoding or the reconstructed image has nans
+#                    if np.isnan(enc).any():
+#                        print('encoding has nan value(s)')
+#                    if np.isnan(dec).any():
+#                        print('decoded frame has nan value(s)')
+
+#                    # check if any of the gradients are nans
+#                    for i, grad in enumerate(gradients):
+#                        print('gradient number {:2d}: max val = {:5.5f}, {:5.5f}. min val = {:5.5f}, {:5.5f}. isnan = {}'.format(i, grad[0].max(), grad[1].max(), grad[0].min(), grad[1].min(), np.isnan(grad).any()))
+
+                    # update summaries
                     summary_writer.add_summary(summary)
 
                     itr += 1
                     if itr % args.log_interval == 0:
-                        print("Itr {} loss = {}".format((epoch)*itr, loss))
+                        print("Epoch {} Itr {} loss = {}".format(epoch, itr, loss))
 
                 print("Done epoch {}".format(epoch))
                 saver.save(