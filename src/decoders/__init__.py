96bf3bf3e20c653a2e0b1c961b51a8b6b5bc27db
nfrumkin <natasha.frumkin@gmail.com>
2018-12-01 12:12:02 -0500

    Add VGG small architectures - update __init__ files to reflect changes - remove extra fully connected layers in e/d - change filter sizes to 8, 16, 32, and 64

--- src/decoders/__init__.py
+++ src/decoders/__init__.py
@@ -1,2 +1,3 @@
 from .vgg_large_decoder import vgg_large_decoder
+from .vgg_small_decoder import vgg_small_decoder
 from .vaegan_decoder import vaegan_decoder
--- src/encoders/__init__.py
+++ src/encoders/__init__.py
@@ -1,2 +1,3 @@
 from .vgg_large_encoder import vgg_large_encoder
+from .vgg_small_encoder import vgg_small_encoder
 from .vaegan_encoder import vaegan_encoder
