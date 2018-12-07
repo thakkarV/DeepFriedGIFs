468fd4bffbbb8bd5d7f530c0a763ce0f6b0e07cc
thakkarv <thakkarv@bu.edu>
2018-12-07 16:11:46 -0500

    add initial full implementation of SerDes for compressed GIFs
    this serializer/deserializer has two static methods for reading and
    writing compressed GIFs to disk as pickle dumps. in the future we can
    refine the impl by using protocol buffers or less prefereably some
    manula binary file format for size optimizations, but pickle dumps will
    do for proof of concept usage.
    
    starts GH-12

--- src/serdes/serdes.py
+++ src/serdes/serdes.py
@@ -1,6 +1,10 @@
 import os
 import os.path as osp
-import sys
+import pickle as pkl
+
+# we define a header for the pickle so we know we are dealing
+# with a file we wrote to disk.
+HEADER = 'DFG'
 
 
 class SerDes(object):
@@ -9,12 +13,59 @@ class SerDes(object):
     embeddings so that can be written, saved and read from disk.
     """
 
-    def __init__(self, window_size, window_offset):
-        self.window_size = int(window_size)
-        self.window_offset = int(window_offset)
+    @staticmethod
+    def read_compressed_gif(path):
+        """Reads a compressed GIF from disk and returns the object.
 
-    def read_compressed_gif(self, path):
-        pass
+        Arguments:
+            path {str} -- Path to where the compressed GIF is stored
 
-    def write_compressed_gif(self, path, head_frames, compressed, tail_frames):
-        pass
+        Returns:
+            obj -- Deserialized GIF as an object.
+        """
+        if not osp.exists(path):
+            print("ERROR: Invalid path {}".format(path))
+        return pkl.load(open(path, 'rb'))
+
+    @staticmethod
+    def write_compressed_gif(
+        path,
+        window_size,
+        window_offset,
+        colour_map,
+        head_frames,
+        compressed_frames,
+        tail_frames):
+        """Writes a compressed GIF to disk as a pickle dump.
+
+        Arguments:
+            path {str} -- Path to where the GIF will be writte
+            window_size {int} -- Window size for compression
+            window_offset {int} -- Window offset used for compression
+            colour_map {None|np.ndarray} -- Nullable colour map of the GIF
+            head_frames {None|np.ndarray} -- Nullable uncompressed head frames
+            compressed_frames {np.ndarray} -- Compressed frames
+            tail_frames {None|np.ndarray} -- Nullable uncompressed tail frames
+        """
+        if osp.exists(path):
+            print("Warning, file at {}\n\talready exists, overrite?\ny/[N]:\t"
+                  .format(path))
+            ans = input()
+            if (ans != 'y') and (ans != 'Y'):
+                print("Aborting")
+                pass
+
+        with open(path, 'wb') as f:
+            pkl.dump(
+                obj=[
+                    HEADER,
+                    window_size,
+                    window_offset,
+                    colour_map,
+                    head_frames,
+                    compressed_frames,
+                    tail_frames
+                ],
+                file=f,
+                protocol=pkl.HIGHEST_PROTOCOL
+            )
